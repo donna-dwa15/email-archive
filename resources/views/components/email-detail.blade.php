dd159edb94c7461d265920833d8be2e8b346852d
donna-dwa15 <donnawwong@gmail.com>
2024-05-21 19:59:42 -0500

    Cleanup and refactoring View Email code

--- resources/views/components/email-detail.blade.php
+++ resources/views/components/email-detail.blade.php
@@ -1,6 +1,6 @@
 @props(['label'])
 
 <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-    <dt class="text-sm font-medium leading-6 text-gray-900">{{ $label }}</dt>
+    <dt class="text-sm font-bold leading-6 text-gray-900">{{ $label }}</dt>
     <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{ $slot }}</dd>
 </div>
