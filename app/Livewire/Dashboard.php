859e24e433193932fbd600d6dfea1156e3418d95
donna-dwa15 <donnawwong@gmail.com>
2024-05-22 15:51:15 -0500

    Updated Dashboard to display email counts

--- app/Livewire/Dashboard.php
+++ app/Livewire/Dashboard.php
@@ -2,12 +2,34 @@
 
 namespace App\Livewire;
 
+use App\Models\Email;
+
 use Livewire\Component;
 
 class Dashboard extends Component
 {
+    public $email_count;
+    
+    public function mount()
+    {
+        // email count to be displayed on the Dashboard
+        $this->email_count = $this->getEmailCount();
+        
+    }
+    
     public function render()
     {
-        return view('livewire.dashboard')->extends('components.layout', ['header' => 'Dashboard']);
+     
+        return view('livewire.dashboard')->extends('components.layout', 
+                ['header' => 'Dashboard']
+            );
+    }
+    
+    /*
+     * Get the current number of emails in the emails db table
+     */
+    private function getEmailCount(): int
+    {
+        return Email::count();
     }
 }
--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,6 +1,5 @@
 <div>
-    <h3 class='font-bold text-lg'>Welcome back to The Email Archive!</h3>
+    <h3 class='font-bold text-lg'>Welcome to The Email Archive!</h3>
     <div class='font-bold text-sm'>
-        <div class='mt-5 mb-5'># of Emails Currently Archived</div>
-        <div class='mt-5 mb-5'>Top 5 Most frequently used tags</div>
+        <div class='mt-5 mb-5'>There are currently {{ $email_count }} emails that have been submitted to the archive.</div>
 </div>
