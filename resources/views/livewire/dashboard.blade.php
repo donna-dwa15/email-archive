cb9f56c0c96480295b125067353b17217de1a537
donna-dwa15 <donnawwong@gmail.com>
2024-05-22 14:20:02 -0500

    Created Dashboard livewire component for main home page

---
+++ resources/views/livewire/dashboard.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <h3 class='font-bold text-lg'>Welcome back to The Email Archive!</h3>
+    <div class='font-bold text-sm'>
+        <div class='mt-5 mb-5'># of Emails Currently Archived</div>
+        <div class='mt-5 mb-5'>Top 5 Most frequently used tags</div>
+</div>
