d3f02f258e0044102036cb9094defaeb78f35394
donna-dwa15 <donnawwong@gmail.com>
2024-05-21 20:17:24 -0500

    Removed index page reference in nav links

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -15,7 +15,7 @@
           <div class="hidden md:block">
             <div class="ml-10 flex items-baseline space-x-4">
               <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-              <x-nav-link href="/index"  wire:navigate :active="request()->is('index')">Dashboard</x-nav-link>
+              <x-nav-link href="/"  wire:navigate :active="request()->is('')">Dashboard</x-nav-link>
               <x-nav-link href="/emails" wire:navigate :active="request()->is('emails')">View</x-nav-link>
               <x-nav-link href="/upload" wire:navigate :active="request()->is('upload')">Upload</x-nav-link>
             </div>
