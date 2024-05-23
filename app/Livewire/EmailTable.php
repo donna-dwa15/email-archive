ae3dc7cc56f7bad217da1f78621a3503ac146841
donna-dwa15 <donnawwong@gmail.com>
2024-05-23 10:02:51 -0500

    Implemented search functionality

--- app/Livewire/EmailTable.php
+++ app/Livewire/EmailTable.php
@@ -11,12 +11,36 @@ class EmailTable extends Component
 {
     use WithPagination;
     
-    public function render()
+    public $searchText = "";
+    
+    /*
+     * Search functionality triggered by input submission on component
+     */
+    public function search()
+    {
+        // Reset any pagination currently done by the user before rendering
+        $this->resetPage();
+        $this->render( );
+    }
+    
+    /*
+     * Retrieve emails by searching on Tag name using specified searchText
+     */
+    private function searchEmailsByTag( )
+    {
+        $emails = Email::whereHas('tags', function($q){
+            $q->where('name', 'LIKE', '%' . $this->searchText . '%');               
+        });
+        
+        return $emails;
+    }
+    
+    public function render( )
     {
 
-        return view('livewire.email-table', [
-            'emails' => Email::with(['tags'])->paginate(10) ,          
-        ])->extends('components.layout', ['header' => 'Archived Emails']);
+        return view('livewire.email-table', 
+            ['emails' => $this->searchEmailsByTag()->paginate(10),    
+            ])->extends('components.layout', ['header' => 'Archived Emails']);
         
     }
 }
